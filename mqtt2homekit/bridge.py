be2f5629fcf5b7ced605a92c07a377fdaa661207
Matthew Schinckel <matt@schinckel.net>
2018-04-08 08:16:32 +0930

    Support multiple services in an accessory.
    This is required for battery status, that cannot be a standalone accessory.

--- mqtt2homekit/bridge.py
+++ mqtt2homekit/bridge.py
@@ -3,18 +3,22 @@ import random
 import signal
 import time
 import uuid
+import logging
 
 import ed25519
 
 from paho.mqtt import client as mqtt
 
-from pyhap import accessory, loader
+from pyhap.accessory import Bridge
+from pyhap.loader import get_serv_loader
 from pyhap.accessory_driver import AccessoryDriver
 from pyhap.util import fromhex, tohex
 
 from accessory import Accessory
 from utils import display_name
 
+
+LOGGER = logging.getLogger(__name__)
 
 ONE_MINUTE = 60
 ONE_HOUR = ONE_MINUTE * 60
@@ -45,7 +49,16 @@ class BridgeEncoder(object):
             # bridge, until we see they still exist on the network. So, we need to
             # store the aid here.
             "accessories": [
-                key.split('/') + [accessory.aid]
+                {
+                    "accessory_id": key,
+                    "name": accessory.display_name,
+                    "services": [
+                        service.display_name
+                        for service in accessory.services
+                        if service.display_name != 'AccessoryInformation'
+                    ],
+                    "aid": accessory.aid
+                }
                 for key, accessory in bridge.known_accessories.items()
             ]
         }
@@ -65,17 +78,15 @@ class BridgeEncoder(object):
         # As mentioned above, we don't want to load the accessories into the
         # bridge because if they have been removed from the network, we don't
         # want to show them in HomeKit.
-        for accessory_id, service_type, aid in state.get('accessories', []):
-            bridge.known_accessories[
-                '{}/{}'.format(accessory_id, service_type)
-            ] = Accessory(
-                display_name(service_type),
-                service=service_type,
-                aid=aid,
+        for accessory in state.get('accessories', []):
+            bridge.known_accessories[accessory['accessory_id']] = Accessory(
+                accessory['name'],
+                aid=accessory['aid'],
+                services=accessory['services']
             )
 
 
-class MQTTBridge(accessory.Bridge):
+class MQTTBridge(Bridge):
     def __init__(self, *args, **kwargs):
         self.persist_file = kwargs.pop('persist_file')
         self.port = random.randint(50000, 60000)
@@ -85,7 +96,7 @@ class MQTTBridge(accessory.Bridge):
         self.known_accessories = {}
 
     def _set_services(self):
-        info_service = loader.get_serv_loader().get("AccessoryInformation")
+        info_service = get_serv_loader().get("AccessoryInformation")
         info_service.get_characteristic("Name")\
                     .set_value('MQTT Bridge', False)
         info_service.get_characteristic("Manufacturer")\
@@ -101,8 +112,9 @@ class MQTTBridge(accessory.Bridge):
         # This will allow us to push onto the MQTT when we get notified by HomeKit that
         # something needs to change.
         super().add_accessory(accessory)
-        for characteristic in accessory.service.characteristics:
-            characteristic.setter_callback = lambda value: self.send_mqtt_message(accessory, characteristic, value)
+        for service in accessory.services:
+            for characteristic in service.characteristics:
+                characteristic.setter_callback = lambda value: self.send_mqtt_message(accessory, service, characteristic, value)
 
     def get_accessory_key(self, accessory):
         for key, known in self.known_accessories.items():
@@ -110,19 +122,28 @@ class MQTTBridge(accessory.Bridge):
                 return key
 
     def get_or_create_accessory(self, accessory_id, service_type):
-        accessory_key = '{}/{}'.format(accessory_id, service_type)
-        if accessory_key not in self.known_accessories:
-            accessory = Accessory(display_name(service_type), service=service_type)
+        if accessory_id not in self.known_accessories:
+            # Need to add this accessory to our registry.
+            # If this is not a valid accessory main service, we should just exit now...
+            accessory = Accessory(display_name(service_type), services=[service_type])
             accessory.set_sentinel(self.run_sentinel)
-            self.known_accessories[accessory_key] = accessory
+            self.known_accessories[accessory_id] = accessory
             self.add_accessory(accessory)
             self.config_changed()
-            self.driver.persist()
+            self.persist()
         else:
-            accessory = self.known_accessories[accessory_key]
+            accessory = self.known_accessories[accessory_id]
+            if not accessory.get_service(service_type):
+                # Need to add this service to this registry.
+                service = get_serv_loader().get(service_type)
+                accessory.add_service(service)
+                accessory.services.append(service)
+                self.config_changed()
+                self.persist()
             if accessory.aid not in self.accessories:
                 self.add_accessory(accessory)
                 self.config_changed()
+                self.persist()
         return accessory
 
     @property
@@ -154,6 +175,9 @@ class MQTTBridge(accessory.Bridge):
         while not self.run_sentinel.wait(ONE_HOUR):
             self.hide_unseen()
 
+    def persist(self):
+        return self.driver.persist()
+
     def hide_unseen(self, since=ONE_DAY):
         # When should we remove them from known_accessories? After FOUR_WEEKS?
         now = time.time()
@@ -163,16 +187,25 @@ class MQTTBridge(accessory.Bridge):
                 self.config_changed()
 
     def handle_mqtt_message(self, client, userdata, message):
+        # TODO: Handle exceptions in here.
         _prefix, accessory_id, service_type, characteristic = message.topic.split('/')
         accessory = self.get_or_create_accessory(accessory_id, service_type)
         value = message.payload.decode('ascii')
-        accessory.set_characteristic(characteristic, value)
+        LOGGER.debug('SET {accessory_id} {service_type} {characteristic} -> {value}'.format(
+            accessory_id=accessory_id,
+            service_type=service_type,
+            characteristic=characteristic,
+            value=value,
+        ))
+        accessory.set_characteristic(service_type, characteristic, value)
         accessory.__last_seen = time.time()
 
-    def send_mqtt_message(self, accessory, characteristic, value):
+    def send_mqtt_message(self, accessory, service, characteristic, value):
+        # TODO: Handle exceptions in here.
         self.client.publish(
-            'HomeKit/{0}/{1}/{characteristic.display_name}'.format(
-                *self.get_accessory_key(accessory).split('/'),
+            'HomeKit/{accessory_id}/{service}/{characteristic.display_name}'.format(
+                accessory_id=self.get_accessory_key(accessory),
+                service=service.display_name,
                 characteristic=characteristic,
             ),
             str(value)
--- mqtt2homekit/main.py
+++ mqtt2homekit/main.py
@@ -1,5 +1,8 @@
+import logging
+
 from bridge import MQTTBridge
 
+logging.basicConfig(level=logging.DEBUG)
 
 bridge = MQTTBridge('MQTT Bridge', persist_file='bridge.state')
 bridge.start(mqtt_server='localhost', mqtt_port=1883, mqtt_timeout=60)
