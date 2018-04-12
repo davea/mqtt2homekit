9b6603836753b74159d8d3e09adda82e7ad42dc3
Matthew Schinckel <matt@schinckel.net>
2018-04-12 23:28:27 +0930

    Fix some issues around persistence.

--- mqtt2homekit/main.py
+++ mqtt2homekit/main.py
@@ -4,5 +4,11 @@ from bridge import MQTTBridge
 
 logging.basicConfig(level=logging.DEBUG)
 
-bridge = MQTTBridge('MQTT Bridge', persist_file='bridge.state')
-bridge.start(mqtt_server='localhost', mqtt_port=1883, mqtt_timeout=60)
+
+def main():
+    bridge = MQTTBridge('MQTT Bridge', persist_file='bridge.state')
+    bridge.start(mqtt_server='localhost', mqtt_port=1883, mqtt_timeout=60)
+
+
+if __name__ == '__main__':
+    main()
