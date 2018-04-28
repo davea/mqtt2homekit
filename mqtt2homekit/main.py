c28f9cda4a936287268d968f58c2bad85dcf735b
Matthew Schinckel <23411+schinckel@users.noreply.github.com>
2018-04-28 20:05:17 +0930

    Change mqtt server.

--- mqtt2homekit/main.py
+++ mqtt2homekit/main.py
@@ -7,7 +7,7 @@ logging.basicConfig(level=logging.DEBUG)
 
 def main():
     bridge = MQTTBridge('MQTT Bridge', persist_file='bridge.state')
-    bridge.start(mqtt_server='localhost', mqtt_port=1883, mqtt_timeout=60)
+    bridge.start(mqtt_server='mqtt.local', mqtt_port=1883, mqtt_timeout=60)
 
 
 if __name__ == '__main__':
