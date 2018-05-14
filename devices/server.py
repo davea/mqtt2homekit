c655f0f88c80377db4686e8751867276687cfd97
Matthew Schinckel <matt@schinckel.net>
2018-05-14 17:12:30 +0930

    Knock-together device setup page.

---
+++ devices/server.py
@@ -0,0 +1,41 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+import json
+
+
+def config_getter(source, key, *keys):
+    if len(keys):
+        return config_getter(source.get(key, {}), *keys)
+    return source.get(key, '')
+
+
+class RequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-Type', 'text/html')
+        self.end_headers()
+
+        config = json.loads(open('config.json').read())
+        data = open('setup.html').read().format(
+            device_id='TESTING',
+            ssid=config_getter(config, 'wifi', 'ssid'),
+            psk=config_getter(config, 'wifi', 'psk'),
+            broker=config_getter(config, 'mqtt', 'broker'),
+            port=config_getter(config, 'mqtt', 'port'),
+            client_id=config_getter(config, 'mqtt', 'client_id'),
+            deep_sleep=config_getter(config, 'deep_sleep'),
+        )
+        self.wfile.write(bytes(data, 'utf8'))
+        return
+
+    def do_POST(self):
+        pass
+
+
+def run():
+    server_address = ('0.0.0.0', 8081)
+    server = HTTPServer(server_address, RequestHandler)
+    server.serve_forever()
+
+
+run()
