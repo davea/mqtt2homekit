a88bae73f05ab97e3c26da0286b932212f2bf886
Matthew Schinckel <23411+schinckel@users.noreply.github.com>
2018-05-19 11:25:16 +0930

    Think about sensors.

--- devices/app.py
+++ devices/app.py
@@ -1,16 +1,19 @@
 # Connect to WiFi.
 from config import config
 import mqtt
-import sensor
 import wifi
 import time
 import esp
 import update
 import machine
 
-wifi.connect(**config.WIFI)
+sensor = None  # To stop Flake8 errors later
 
+# Try to connect to wifi: if we can't (or we aren't configured), then we want
+# to start up our access point radio, and start broadcasting that for config.
+wifi.connect(**config.WIFI)
 if not wifi.connected():
+    wifi.start_ap()
     config.setup()
 
 try:
@@ -19,10 +22,16 @@ try:
 except mqtt.MQTTException:
     config.setup()
 
+try:
+    exec('import sensors.{} as sensor'.format(config.SENSOR))
+except ImportError:
+    config.setup()
+
 # See if we need to apply any updates to the system.
 client.set_callback(update.apply_update)
-client.subscribe('updates/{}'.format(sensor.SENSOR_ID))
+client.subscribe('updates/{}'.format(sensor.SENSOR))
 if client.check_msg():
+    # Do we need to do anything to make sure the callback fires before we reset?
     machine.reset()
 
 # If not, then we need to read the sensor data, and send it to the MQTT broker.
